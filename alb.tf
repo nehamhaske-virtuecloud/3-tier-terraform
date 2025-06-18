724c86b205bae35f8824feef890263f0201b3e41
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-18 22:08:49 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,13 +1,8 @@
 resource "aws_lb" "alb" {
   name               = "${var.project_name}-alb"
-  internal           = false
   load_balancer_type = "application"
+  subnets            = aws_subnet.public.*.id
   security_groups    = [aws_security_group.web_sg.id]
-  subnets            = [aws_subnet.public.id]
-
-  tags = {
-    Name = "${var.project_name}-alb"
-  }
 }
 
 resource "aws_lb_target_group" "tg" {
@@ -16,25 +11,13 @@ resource "aws_lb_target_group" "tg" {
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
 
-  health_check {
-    path                = "/"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    protocol            = "HTTP"
-  }
-
-  tags = {
-    Name = "${var.project_name}-tg"
-  }
+  health_check { path = "/", protocol = "HTTP" }
 }
 
 resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_lb.alb.arn
   port              = 80
   protocol          = "HTTP"
-
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.tg.arn
