ee9d46af77112ce46fd217729f4265e121689f9d
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-18 22:38:25 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,59 +1,77 @@
+# ------------------------
+# General Project Settings
+# ------------------------
+
 variable "project_name" {
-  description = "Project name"
+  description = "Project name used for tagging AWS resources"
   type        = string
 }
 
+# ------------------------
+# Networking Configuration
+# ------------------------
+
 variable "vpc_cidr_block" {
-  description = "CIDR block for the VPC"
+  description = "CIDR block for the main VPC"
   type        = string
 }
 
 variable "public_subnet_cidr" {
-  description = "List of public subnet CIDRs"
+  description = "List of CIDR blocks for public subnets (e.g., web tier)"
   type        = list(string)
 }
 
 variable "private_subnet_cidr_app" {
-  description = "List of private subnet CIDRs for the app tier"
+  description = "List of CIDR blocks for private subnets (e.g., app tier)"
   type        = list(string)
 }
 
 variable "private_subnet_cidr_db" {
-  description = "List of private subnet CIDRs for the db tier"
+  description = "List of CIDR blocks for private subnets (e.g., db tier)"
   type        = list(string)
 }
 
 variable "availability_zones" {
-  description = "List of availability zones"
+  description = "List of availability zones (e.g., [\"ap-south-1a\", \"ap-south-1b\"])"
   type        = list(string)
 }
 
+# ------------------------
+# EC2 Settings
+# ------------------------
+
 variable "instance_type" {
+  description = "EC2 instance type (e.g., t2.micro)"
   type        = string
-  description = "EC2 instance type"
 }
 
 variable "key_name" {
+  description = "Name of the EC2 key pair"
   type        = string
-  description = "EC2 Key Pair name"
 }
 
 variable "allowed_ip" {
+  description = "Public IP or CIDR block allowed to SSH/HTTP (e.g., 0.0.0.0/0 or your IP)"
   type        = string
-  description = "Allowed IP for inbound access"
 }
 
+# ------------------------
+# RDS Settings
+# ------------------------
+
 variable "db_username" {
+  description = "Master username for the RDS instance"
   type        = string
-  description = "Username for the RDS database"
+  sensitive   = true
 }
 
 variable "db_password" {
+  description = "Master password for the RDS instance"
   type        = string
-  description = "Password for the RDS database"
+  sensitive   = true
 }
 
 variable "db_name" {
+  description = "Name of the RDS database to create"
   type        = string
-  description = "Name of the RDS database"
 }
