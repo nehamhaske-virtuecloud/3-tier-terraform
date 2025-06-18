45fc81564ed1cb0bfb8e7da5562660d2e103607c
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-18 22:15:05 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,31 +1,51 @@
 resource "aws_lb" "alb" {
   name               = "${var.project_name}-alb"
+  internal           = false
   load_balancer_type = "application"
-  subnets            = aws_subnet.public.*.id
-  security_groups    = [aws_security_group.web_sg.id]
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public[0].id, aws_subnet.public[1].id]
+
+  tags = {
+    Name = "${var.project_name}-alb"
+  }
 }
 
 resource "aws_lb_target_group" "tg" {
-  name     = "${var.project_name}-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+  name        = "${var.project_name}-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "instance"
 
-  health_check { path = "/", protocol = "HTTP" }
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "${var.project_name}-tg"
+  }
 }
 
-resource "aws_lb_listener" "listener" {
+resource "aws_lb_listener" "alb_listener" {
   load_balancer_arn = aws_lb.alb.arn
   port              = 80
   protocol          = "HTTP"
+
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.tg.arn
   }
 }
 
-resource "aws_lb_target_group_attachment" "web_attachment" {
+resource "aws_lb_target_group_attachment" "web_attachments" {
+  count            = length(aws_instance.web_instance[*].id)
   target_group_arn = aws_lb_target_group.tg.arn
-  target_id        = aws_instance.nginx_proxy.id
+  target_id        = aws_instance.web_instance[count.index].id
   port             = 80
 }
