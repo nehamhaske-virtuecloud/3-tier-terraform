0077729955b433c699dd0ad243ce4149896e3e03
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-18 19:14:58 +0530

    Update web-tier.tf

--- web-tier.tf
+++ web-tier.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "nginx_proxy" {
   ami = data.aws_ami.ubuntu.id
   instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.public[0].id
+  subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   associate_public_ip_address = true
