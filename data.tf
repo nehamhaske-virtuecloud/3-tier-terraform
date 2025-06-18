17cf7320cd0b590714fa06767050482ec317401f
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-18 19:34:08 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,11 +1,15 @@
-# Get the latest Ubuntu AMI from Canonical
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners = ["amazon"]
+  owners      = ["099720109477"] # Canonical
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-24.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 
   filter {
