ff8c055c32a111d97a0a10b5434d6e92f76ffe09
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-18 21:52:00 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,4 @@
 variable "project_name" {
-  description = "Project name prefix for tagging"
-  type        = string
-}
-
-variable "vpc_cidr_block" {
-  description = "CIDR block for the VPC"
-  type        = string
-}
-
-variable "project_name" {
   description = "Name prefix for all resources"
   type        = string
   default     = "neha-3tier"
@@ -20,7 +10,6 @@ variable "vpc_cidr_block" {
   default     = "10.0.0.0/16"
 }
 
-# Availability Zones
 variable "availability_zone_1" {
   description = "Primary Availability Zone"
   type        = string
@@ -28,43 +17,73 @@ variable "availability_zone_1" {
 }
 
 variable "availability_zone_2" {
-  description
+  description = "Secondary Availability Zone"
+  type        = string
+  default     = "ap-south-1b"
+}
 
+# Public Subnets
+variable "public_subnet_cidr_1" {
+  description = "CIDR block for Public Subnet 1"
+  type        = string
+  default     = "10.0.1.0/24"
+}
 
-# variable "ami_id" {
-# description = "AMI ID for EC2 instances"
-# type        = string
-# }
+variable "public_subnet_cidr_2" {
+  description = "CIDR block for Public Subnet 2"
+  type        = string
+  default     = "10.0.2.0/24"
+}
 
+# Private Subnets
+variable "private_subnet_cidr_1" {
+  description = "CIDR block for Private Subnet 1"
+  type        = string
+  default     = "10.0.3.0/24"
+}
+
+variable "private_subnet_cidr_2" {
+  description = "CIDR block for Private Subnet 2"
+  type        = string
+  default     = "10.0.4.0/24"
+}
+
+# Instance Type
 variable "instance_type" {
-  description = "Instance type for EC2"
+  description = "EC2 instance type"
   type        = string
+  default     = "t2.micro"
 }
 
+# SSH Key
 variable "key_name" {
-  description = "Key pair name for EC2 SSH"
+  description = "Key pair name to access EC2"
   type        = string
+  default     = "my-new-key"
 }
 
+# CIDR for SSH Access
 variable "allowed_ip" {
-  description = "Allowed CIDR for SSH and HTTP access"
+  description = "Your IP address to SSH into the EC2 instance"
   type        = string
+  default     = "0.0.0.0/0"
 }
 
+# RDS Configuration
 variable "db_username" {
-  description = "RDS database username"
+  description = "Username for RDS DB"
   type        = string
+  default     = "admin"
 }
 
 variable "db_password" {
-  description = "RDS database password"
+  description = "Password for RDS DB"
   type        = string
-  sensitive   = true
+  default     = "password123!"
 }
 
 variable "db_name" {
-  description = "RDS database name"
+  description = "Database name for RDS"
   type        = string
+  default     = "mydb"
 }
-
-
