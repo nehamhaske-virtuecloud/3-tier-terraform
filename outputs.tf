63882c02d2d32df969390ced02ce870e21468803
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-18 18:32:14 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,33 @@
-output "alb_dns_name" {
-  value = aws_lb.app_alb.dns_name
+output "vpc_id" {
+  description = "ID of the created VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public.id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private.id
+}
+
+output "web_instance_public_ip" {
+  description = "Public IP of the Web EC2 instance"
+  value       = aws_instance.web.public_ip
+}
+
+output "app_instance_private_ip" {
+  description = "Private IP of the App EC2 instance"
+  value       = aws_instance.app.private_ip
+}
+
+output "alb_dns" {
+  description = "ALB DNS Name"
+  value       = aws_lb.alb.dns_name
 }
 
 output "rds_endpoint" {
-  value = aws_db_instance.mysql.endpoint
+  description = "RDS Endpoint"
+  value       = aws_db_instance.db.endpoint
 }
+
