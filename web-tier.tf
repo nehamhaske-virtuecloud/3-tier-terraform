d5cf1595fc62de71309e1ad25be02c304017dc36
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-18 22:28:07 +0530

    Update web-tier.tf

--- web-tier.tf
+++ web-tier.tf
@@ -1,14 +1,10 @@
 resource "aws_instance" "web_instance" {
-  count                     = 2
-  ami                       = data.aws_ami.ubuntu.id
-  instance_type             = var.instance_type
-  subnet_id                 = values(aws_subnet.public)[count.index].id
-  key_name                  = var.key_name
-  vpc_security_group_ids    = [aws_security_group.web_sg.id]
-
-  tags = {
-    Name = "${var.project_name}-web-${count.index + 1}"
-  }
+  count                  = 2
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  subnet_id              = values(aws_subnet.public)[count.index].id
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -19,7 +15,7 @@ resource "aws_instance" "web_instance" {
               server {
                   listen 80;
                   location / {
-                      proxy_pass http://${aws_instance.app_instance.private_ip}:8080/;
+                      proxy_pass http://${aws_instance.app_instance[0].private_ip}:8080/;
                       proxy_set_header Host \$host;
                       proxy_set_header X-Real-IP \$remote_addr;
                       proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
@@ -32,7 +28,8 @@ resource "aws_instance" "web_instance" {
               systemctl enable nginx
               EOF
 
- tags = {
-    Name = "${var.project_name}-web-${count.index + 1}"
+  tags = {
+    Name        = "${var.project_name}-web-${count.index + 1}"
+    Environment = "dev"
   }
 }
