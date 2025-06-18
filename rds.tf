a3fd4e5717c77f4bdb2cc45731bf11a866c728da
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-18 22:09:22 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,32 +1,21 @@
-resource "aws_db_subnet_group" "rds_subnet_group" {
+resource "aws_db_subnet_group" "db_subnet_group" {
   name       = "${var.project_name}-db-subnet-group"
-  subnet_ids = [
-  aws_subnet.private_subnet_1.id,
-  aws_subnet.private_subnet_2.id
-]
-
-
-  tags = {
-    Name = "${var.project_name}-db-subnet-group"
-  }
+  subnet_ids = aws_subnet.db_private.*.id
+  tags       = { Name = "${var.project_name}-db-subnet-group" }
 }
 
 resource "aws_db_instance" "db" {
-  identifier              = "${var.project_name}-db"
-  engine                  = "mysql"
-  instance_class          = "db.t3.micro"
-  allocated_storage       = 20
-  username                = var.db_username
-  password                = var.db_password
-  db_name                 = var.db_name
-  vpc_security_group_ids  = [aws_security_group.rds_sg.id]
-  db_subnet_group_name    = aws_db_subnet_group.rds_subnet_group.name
-  skip_final_snapshot     = true
-  publicly_accessible     = false
-  multi_az                = false
-  deletion_protection     = false
-
-  tags = {
-    Name = "${var.project_name}-rds"
-  }
+  identifier             = "${var.project_name}-db"
+  engine                 = "mysql"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 20
+  name                   = var.db_name
+  username               = var.db_username
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  skip_final_snapshot    = true
+  publicly_accessible    = false
+  multi_az               = false
+  deletion_protection    = false
 }
