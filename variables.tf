c763017147d59800b84361e9fdf16f8b9c05a819
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-18 17:25:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -19,3 +19,33 @@ variable "region" {
   type        = string
   default     = "ap-south-1" # Mumbai (change if needed)
 }
+
+
+#
+variable "region" { type = string }
+variable "cluster_name" { type = string }
+variable "application_environment" { type = string }
+# variable "public_subnet1" { type = string }
+# variable "public_subnet2" { type = string }
+# variable "public_subnet3" { type = string }
+# variable "private_subnet1" { type = string }
+# variable "private_subnet2" { type = string }
+# variable "private_subnet3" { type = string }
+
+# variable "vpc_id" { type = string }
+
+variable "desired_max_size" { type = number }
+variable "desired_size" { type = number }
+variable "desired_min_size" { type = number }
+variable "target_cpu_usage" { type = number }
+variable "minimum_scaling_step_size" { type = number }
+variable "maximum_scaling_step_size" { type = number }
+
+#ec2
+variable "instance_name" {}
+variable "instance_type" {}
+variable "key_name" {}
+variable "ec2_option" {
+  type = bool
+  default = false
+}
