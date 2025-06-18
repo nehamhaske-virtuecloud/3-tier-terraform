587d74bd258956545a064535f4012e1f5dfe0464
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-18 18:30:17 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,59 +1,62 @@
-variable "vpc_cidr" {
-  description = "Main VPC ID CIDR"
+variable "project_name" {
+  description = "Project name prefix for tagging"
   type        = string
-  default = "10.0.0.0/16"
 }
 
-variable "public_subnet_cidrs" {
-  description = "Main VPC public subnet CIDR"
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
   type        = string
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
-variable "private_app_subnet_cidrs" {
-  description = "Main VPC private application subnet CIDR"
+variable "public_subnet_cidr" {
+  description = "CIDR block for public subnet"
   type        = string
-  default = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
-variable "private_db_subnet_cidrs" {
-  description = "Main VPC private database subnet CIDR"
+variable "private_subnet_cidr" {
+  description = "CIDR block for private subnet"
   type        = string
-  default = ["10.0.5.0/24", "10.0.6.0/24"]
 }
 
-variable "region" {
-  description = "AWS region to deploy resources"
+variable "availability_zone" {
+  description = "AWS Availability Zone"
   type        = string
-  default     = "ap-south-1" # Mumbai (change if needed)
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type for EC2"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Key pair name for EC2 SSH"
+  type        = string
+}
+
+variable "allowed_ip" {
+  description = "Allowed CIDR for SSH and HTTP access"
+  type        = string
+}
+
+variable "db_username" {
+  description = "RDS database username"
+  type        = string
+}
+
+variable "db_password" {
+  description = "RDS database password"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_name" {
+  description = "RDS database name"
+  type        = string
 }
 
 
-#
-variable "region" { type = string }
-variable "cluster_name" { type = string }
-variable "application_environment" { type = string }
-# variable "public_subnet1" { type = string }
-# variable "public_subnet2" { type = string }
-# variable "public_subnet3" { type = string }
-# variable "private_subnet1" { type = string }
-# variable "private_subnet2" { type = string }
-# variable "private_subnet3" { type = string }
-
-# variable "vpc_id" { type = string }
-
-variable "desired_max_size" { type = number }
-variable "desired_size" { type = number }
-variable "desired_min_size" { type = number }
-variable "target_cpu_usage" { type = number }
-variable "minimum_scaling_step_size" { type = number }
-variable "maximum_scaling_step_size" { type = number }
-
-#ec2
-variable "instance_name" {}
-variable "instance_type" {}
-variable "key_name" {}
-variable "ec2_option" {
-  type = bool
-  default = false
-}
