89a0f568eb6793df3615d170ee90e018a5b0c8a5
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-18 22:25:26 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -9,7 +9,7 @@ resource "aws_db_instance" "db" {
   engine                 = "mysql"
   instance_class         = "db.t3.micro"
   allocated_storage      = 20
-  name                   = var.db_name
+  db_name               = var.db_name   
   username               = var.db_username
   password               = var.db_password
   db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
