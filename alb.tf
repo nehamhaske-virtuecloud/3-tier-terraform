aeab562c71e442134a90e02a890fbeb939a91f2b
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-18 18:27:43 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,46 +1,48 @@
-resource "aws_lb" "app_alb" {
-  name               = "app-alb"
+resource "aws_lb" "alb" {
+  name               = "${var.project_name}-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = [aws_subnet.public[0].id, aws_subnet.public[1].id]
+  security_groups    = [aws_security_group.web_sg.id]
+  subnets            = [aws_subnet.public.id]
 
   tags = {
-    Name = "app-alb"
+    Name = "${var.project_name}-alb"
   }
 }
 
-resource "aws_lb_target_group" "app_tg" {
-  name        = "app-tg"
-  port        = 8080
-  protocol    = "HTTP"
-  vpc_id      = aws_vpc.main.id
-  target_type = "instance"
+resource "aws_lb_target_group" "tg" {
+  name     = "${var.project_name}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
 
   health_check {
-    enabled             = true
-    interval            = 30
     path                = "/"
+    interval            = 30
     timeout             = 5
     healthy_threshold   = 2
     unhealthy_threshold = 2
-    matcher             = "200"
+    protocol            = "HTTP"
+  }
+
+  tags = {
+    Name = "${var.project_name}-tg"
   }
 }
 
-resource "aws_lb_listener" "alb_listener" {
-  load_balancer_arn = aws_lb.app_alb.arn
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.alb.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.app_tg.arn
+    target_group_arn = aws_lb_target_group.tg.arn
   }
 }
 
-resource "aws_lb_target_group_attachment" "app_attachment" {
-  target_group_arn = aws_lb_target_group.app_tg.arn
-  target_id        = aws_instance.app_instance.id
-  port             = 8080
+resource "aws_lb_target_group_attachment" "web_attachment" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.web.id
+  port             = 80
 }
