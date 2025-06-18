72b6dd1594200ed5df093f3dd96a616666113d4c
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-18 21:25:36 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,29 +1,9 @@
-data "aws_ami" "my_ami" {
+data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["amazon"]
+  owners      = ["099720109477"]
 
   filter {
-    name   = "name"
-    values = ["al2023-ami-ecs-hvm-*-kernel-6.1-x86_64"] #dev - ami-00889ad21b50fe113
-  }
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "ena-support"
-    values = ["true"]
+    name   = "image-id"
+    values = ["ami-0f918f7e67a3323f0"]
   }
 }
