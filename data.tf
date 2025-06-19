82b8b4c6968439a45b210f8c72a02e64f57c9aa3
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-19 16:40:58 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -3,7 +3,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20250610"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
 
   filter {
