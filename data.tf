9409fefb1af77a8c96410308bf0993e8299f624d
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-19 16:27:52 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,9 +1,25 @@
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["099720109477"]
 
   filter {
-    name   = "image-id"
-    values = ["ami-0f918f7e67a3323f0"]
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*"]
   }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  owners = ["099720109477"] # Canonical (Ubuntu)
 }
