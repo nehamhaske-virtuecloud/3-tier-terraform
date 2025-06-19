c8ccf429c6d0d3c44ea0b61564a476e9c7ce5cf9
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-19 16:47:00 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,16 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+provider "aws" {
+  region = var.region
+  default_tags {
+    tags = {
+      "Project" : "neha-3tier"
+      "Environment" : "Dev"
     }
   }
-
-  required_version = ">= 1.3.0"
-
-  backend "local" {}
-}
-
-provider "aws" {
-  region = "ap-south-1"
 }
